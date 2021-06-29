54baf875f61d43dbab513712818c1053a3cc7bf7
maohuihua <huihuamao@whut.edu.cn>
2021-06-29 15:55:35 +0800

    修改了几处bug

--- src/main/java/com/mao/demo/controller/WatermarkController.java
+++ src/main/java/com/mao/demo/controller/WatermarkController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
 import java.io.*;
 import java.net.URLEncoder;
 
@@ -21,24 +23,25 @@ public class WatermarkController {
     @PostMapping(value = "/embed")
     @ResponseBody
     public void addWaterMark(MultipartFile image, boolean isText, String watermark, HttpServletResponse response)  {
-        // 1、获取文件名后缀
-        String fileName = image.getOriginalFilename();
-        String[] split = fileName.split("\\.");
-        // 2、创建临时文件
-        String prefix = "image";
-        String suffix = ".png";
-        String imageSuffix = "." + split[split.length-1];
         try {
-            // 3、输入图片、输出图片、二维码图片的临时文件
-            File tempInputFile = File.createTempFile(prefix, imageSuffix);
-            File tempOutputFile = File.createTempFile(prefix,suffix);
-            File tempOutputQRFile = File.createTempFile(prefix,suffix);
-            // 临时存储输入文件
+            // 检查上传的图片，如果不符合要求 则终止
+            if (checkImage(image, isText)) {
+                System.out.println("图片格式不正确或不符合要求");
+                return;
+            }
+            // 1、获取文件名后缀
+            String fileName = image.getOriginalFilename();
+            // 2、创建临时文件：输入图片、输出图片、二维码图片的临时文件
+            File tempInputFile = File.createTempFile("image", fileName);
+            File tempOutputFile = File.createTempFile("image",".png");
+            File tempOutputQRFile = File.createTempFile("image",".png");
+
             image.transferTo(tempInputFile);
+            // 3.获取文件路径
             String imagePath = tempInputFile.getAbsolutePath();
             String outputPath = tempOutputFile.getAbsolutePath();
             String outputQR = tempOutputQRFile.getAbsolutePath();
-            // 开始加水印 文本则使用DFT, 否则使用DCT
+            // 4.开始加水印 文本则使用DFT, 否则使用DCT
             if (isText){
                 WaterMarkDFT.embed(imagePath, watermark, outputPath);
             }else{
@@ -46,12 +49,15 @@ public class WatermarkController {
                 new QRCodeUtils().QREncode(watermark, outputQR);
                 WaterMarkDCT.embed(imagePath, outputQR, outputPath);
             }
-            // 返回处理后的文件
-            sendImageBack(response,outputPath);
-            // 5.删除临时文件
-            tempInputFile.delete();
-            tempOutputFile.delete();
-            tempOutputQRFile.delete();
+            // 5.返回处理后的文件
+            download(response,outputPath);
+            // 6.删除临时文件
+            boolean delete = tempInputFile.delete();
+            boolean delete1 = tempOutputFile.delete();
+            boolean delete2 = tempOutputQRFile.delete();
+            if (delete && delete1 && delete2){
+                System.out.println("临时文件已删除");
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -60,17 +66,16 @@ public class WatermarkController {
     @PostMapping(value = "/extract",produces = {MediaType.IMAGE_PNG_VALUE})
     @ResponseBody
     public byte[] getWaterMark(MultipartFile picture, boolean isText)  {
-        // 获取文件名后缀
-        String fileName = picture.getOriginalFilename();
-        String[] split = fileName.split("\\.");
-        // 临时文件前缀和后缀
-        String prefix = "temp";
-        String suffix = ".png";
-        String imageSuffix = "." + split[split.length-1];
-        byte[] bytes = null;
         try {
-            File tempInputFile = File.createTempFile(prefix, imageSuffix);
-            File tempOutputFile = File.createTempFile(prefix,suffix);
+            if (checkImage(picture, isText)) {
+                String str = "图片格式不正确或不符合要求";
+                System.out.println(str);
+                return null;
+            }
+
+            String fileName = picture.getOriginalFilename();
+            File tempInputFile = File.createTempFile("temp", fileName);
+            File tempOutputFile = File.createTempFile("temp",".png");
             picture.transferTo(tempInputFile);
             String imagePath = tempInputFile.getAbsolutePath();
             String outputPath = tempOutputFile.getAbsolutePath();
@@ -82,19 +87,39 @@ public class WatermarkController {
             // 返回图片
             File file = new File(outputPath);
             FileInputStream inputStream = new FileInputStream(file);
-            bytes = new byte[inputStream.available()];
-            inputStream.read(bytes, 0, inputStream.available());
+            byte[] bytes  = new byte[inputStream.available()];
+            int length = inputStream.read(bytes, 0, inputStream.available());
             // 关闭流、删除临时文件
             inputStream.close();
-            tempInputFile.delete();
-            tempOutputFile.delete();
+            boolean delete0 = tempInputFile.delete();
+            boolean delete1 = tempOutputFile.delete();
+            if (delete0 && delete1){
+                System.out.println("临时文件已删除");
+            }
+            return bytes;
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
-        return bytes;
     }
 
-    private void sendImageBack(HttpServletResponse response, String filePath) throws Exception{
+    private boolean checkImage(MultipartFile image, boolean isText) throws IOException {
+        Image picture = ImageIO.read(image.getInputStream());
+        // 如果不是图片, 返回false
+        if (picture == null){
+            return true;
+        }
+        // 是图片，则判断水印类别以及要求的图片尺寸
+        if (isText){
+            return false;
+        } else {
+            // (二维码水印 && 图片尺寸 >= 800 * 800 返回true)
+            return (picture.getWidth(null) < 800) || (picture.getHeight(null) < 800);
+        }
+    }
+
+
+    private void download(HttpServletResponse response, String filePath) throws Exception{
         File file = new File(filePath);
         // 1、设置response 响应头
         // 设置页面不缓存,清空buffer
